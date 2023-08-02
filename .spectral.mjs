4b8c2d2702bb9c65b30c3b3adbd8cb08b850fe4e
Madhukar Pokala <pokalamadhukar@gmail.com>
2023-08-02 17:11:20 +0530

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxMjgzOQ/spectral.js?branch_slug=master&token=cc291229-5ab5-4377-8685-0a0404154ac4";
+export default { extends: ruleset };
